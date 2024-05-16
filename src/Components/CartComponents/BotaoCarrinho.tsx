83ad44d4f62c4f19ebf6f5ef3cac60f36f9e98d9
Ramon Gaspar Fernandez <rahlive21@gmail.com>
2024-05-16 16:14:33 -0300

    Update BotaoCarrinho.tsx

--- src/Components/CartComponents/BotaoCarrinho.tsx
+++ src/Components/CartComponents/BotaoCarrinho.tsx
@@ -19,7 +19,7 @@ function BotaoCarrinho() {
         
         <div className='b-carrinho'>
             <div onClick={handleClick} className='b-carrinho-content' >
-                <img src='https://github.com/ramongaspar/MKS-Challenge/blob/main/src/assets/images/carrinho_compras.png'></img>
+                <img src='https://raw.githubusercontent.com/ramongaspar/MKS-Challenge/main/src/assets/images/carrinho_compras.png'></img>
                 <h3>{quantidade.toString()}</h3>
             </div>
             <CarrinhoCompras closeBar={setTopBar} barStatus={topBar}></CarrinhoCompras>
